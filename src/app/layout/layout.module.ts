1fbc2baf66134b559c5806e67a72c600619022c7
ariribeiro <arivaldoribeiro@gmail.com>
2020-08-16 13:59:39 -0300

    add sidenav

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,3 +1,4 @@
+import { SidenavModule } from './sidenav/sidenav.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
@@ -5,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [LayoutComponent],
@@ -15,7 +17,10 @@ import { MatIconModule } from '@angular/material/icon';
     // Material angular
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+
+    SidenavModule
   ],
   exports: [LayoutComponent]
 })
